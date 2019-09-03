2c59d32ff001768952ece05957a58a89a8ab3fc9
Contour Lines <stevenhang30@gmail.com>
2019-09-03 22:48:47 +1000

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+var Hello = (function () {
+    function Hello() {
+    }
+    return Hello;
+}());
+exports["default"] = Hello;
+//# sourceMappingURL=index.js.map
