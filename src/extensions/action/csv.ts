e2eb8a111c37a56a97e7c1b9a12ae9398e027db6
Contour Lines <stevenhang30@gmail.com>
2019-09-27 00:18:08 +1000

    Added csv action

---
+++ src/extensions/action/csv.ts
@@ -0,0 +1,26 @@
+import neatCsv from 'neat-csv';
+import fs from 'fs';
+import { promisify } from 'util';
+import { HurdleAction } from '../../action/action';
+import { RunnerState } from '../../runner/state';
+
+/**
+ * Csv Action to convert a csv file to an array of objects
+ */
+export class CsvActionHurdleProject implements HurdleAction {
+  static id = 'csv';
+  properties: any = {
+    fileLocation: ''
+  };
+
+  async execute(state: RunnerState): Promise<object> {
+    try {
+      const readFileAsync = promisify(fs.readFile);
+      const data = await readFileAsync(this.properties.fileLocation);
+      return neatCsv(data);
+    } catch (error) {
+      console.log(error);
+    }
+    return {};
+  }
+}
