6387d4babb65dd94868d3724710c681a0896d370
James <jjdikha@gmail.com>
2019-09-26 22:16:22 +1000

    Handle json data in http action

--- src/extensions/action/http.ts
+++ src/extensions/action/http.ts
@@ -12,7 +12,32 @@ export class HtppActionHurdleProject implements HurdleAction {
   async execute(state: RunnerState): Promise<object> {
     try {
       const response = await got(this.properties);
-      return response;
+      let data = {};
+
+      let contentType = response.headers["content-type"];
+      if (contentType && contentType.includes('application/json')) {
+        data = JSON.parse(response.body);
+        contentType = 'application/json';
+      } else if (contentType && (contentType.includes('text/xml') || contentType.includes('application/xml'))) {
+        contentType = 'application/xml';
+        // handle xml
+      }
+
+      return {
+        body: response.body,
+        headers: response.headers,
+        data: data,
+        statusCode: response.statusCode,
+        statusMessage: response.statusMessage,
+        url: response.url,
+        retryCount: response.retryCount,
+        httpVersion: response.httpVersion,
+        httpVersionMajor: response.httpVersionMajor,
+        httpVersionMinor: response.httpVersionMinor,
+        rawHeaders: response.rawHeaders,
+        redirectUrls: response.redirectUrls,
+        contentType: contentType
+      };
     } catch (error) {
       console.log(error.response.body);
     }
