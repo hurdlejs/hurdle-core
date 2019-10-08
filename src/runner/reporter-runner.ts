6e1780d993301e57ba9d4068cc4d89a50698639a
Contour Lines <stevenhang30@gmail.com>
2019-10-08 21:59:52 +1100

    Fixed up build issues

--- src/runner/reporter-runner.ts
+++ src/runner/reporter-runner.ts
@@ -4,58 +4,60 @@ import { TestSection, TestCase, TestStep, TestAssertion } from '../project/proje
 
 export class ReporterRunner {
   queue: QueueReportAction[] = [];
-  reporters: HurdleReporter[];
+  reporters: HurdleReporter[] = [];
   constructor(reporters: HurdleReporter[]) {
     this.reporters = reporters;
     setInterval(this.execute, 1000);
   }
 
   execute() {
-    while (queue.length > 0) {
+    while (this.queue.length > 0) {
       let obj = this.queue.shift();
-      const { runnerTime, testSection, testCase, testStep, testCheck } = { ...obj.properties };
+      if (obj) {
+        let { runnerTime = {}, testSection = {}, testCase = {}, testStep = {}, testCheck = {} } = { ...obj.properties };
 
-      switch (obj.action) {
-        case ReportAction.projecStart:
-          this.reporters.forEach(reporter => reporter.projectStart(runnerTime));
-          break;
-        case ReportAction.projectEnd:
-          this.reporters.forEach(reporter => reporter.projectEnd(runnerTime));
-          break;
-        case ReportAction.sectionStart:
-          this.reporters.forEach(reporter => reporter.sectionStart(testSection, runnerTime));
-          break;
-        case ReportAction.sectionEnd:
-          this.reporters.forEach(reporter => reporter.sectionEnd(testSection, runnerTime));
-          break;
-        case ReportAction.caseStart:
-          this.reporters.forEach(reporter => reporter.caseStart(testCase, runnerTime));
-          break;
-        case ReportAction.caseEnd:
-          this.reporters.forEach(reporter => reporter.caseEnd(testCase, runnerTime));
-          break;
-        case ReportAction.testStart:
-          this.reporters.forEach(reporter => reporter.testStart(testStep, runnerTime));
-          break;
-        case ReportAction.testEnd:
-          this.reporters.forEach(reporter => reporter.testEnd(testStep, runnerTime));
-          break;
-        case ReportAction.testPass:
-          this.reporters.forEach(reporter => reporter.testPass(testStep, testCheck));
-          break;
-        case ReportAction.testFail:
-          this.reporters.forEach(reporter => reporter.testFail(testStep, testCheck));
-          break;
-        default:
-          //improper queued object should never happen
-          throw Error('Impossible');
-          break;
+        switch (obj.action) {
+          case ReportAction.projectStart:
+            this.reporters.forEach(reporter => reporter.projectStart(runnerTime));
+            break;
+          case ReportAction.projectEnd:
+            this.reporters.forEach(reporter => reporter.projectEnd(runnerTime));
+            break;
+          case ReportAction.sectionStart:
+            this.reporters.forEach(reporter => reporter.sectionStart(testSection, runnerTime));
+            break;
+          case ReportAction.sectionEnd:
+            this.reporters.forEach(reporter => reporter.sectionEnd(testSection, runnerTime));
+            break;
+          case ReportAction.caseStart:
+            this.reporters.forEach(reporter => reporter.caseStart(testCase, runnerTime));
+            break;
+          case ReportAction.caseEnd:
+            this.reporters.forEach(reporter => reporter.caseEnd(testCase, runnerTime));
+            break;
+          case ReportAction.testStart:
+            this.reporters.forEach(reporter => reporter.testStart(testStep, runnerTime));
+            break;
+          case ReportAction.testEnd:
+            this.reporters.forEach(reporter => reporter.testEnd(testStep, runnerTime));
+            break;
+          case ReportAction.testPass:
+            this.reporters.forEach(reporter => reporter.testPass(testStep, testCheck));
+            break;
+          case ReportAction.testFail:
+            this.reporters.forEach(reporter => reporter.testFail(testStep, testCheck));
+            break;
+          default:
+            //improper queued object should never happen
+            throw Error('Impossible');
+            break;
+        }
       }
     }
   }
 
   queueProjectStart(runnerTime: RunnerTime) {
-    queue.push({ action: ReportAction.projectStart, properties: { runnerTime } });
+    this.queue.push({ action: ReportAction.projectStart, properties: { runnerTime } });
   }
 }
 
